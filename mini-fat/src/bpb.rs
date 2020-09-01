01f4a0979309bfd34bae9afb368ca49e95311da9
Stephen Sherratt <stephen@sherra.tt>
2020-09-02 09:22:05 +1000

    Start more minimal bpb

--- mini-fat/src/bpb.rs
+++ mini-fat/src/bpb.rs
@@ -1,5 +1,34 @@
 use crate::directory::{Directory, DIRECTORY_ENTRY_BYTES};
 
+pub struct Bpb2 {
+    jmp_boot: [u8; 3],
+    oem_name: String,
+    bytes_per_sector: u16,
+    sectors_per_cluster: u8,
+    reserved_sector_count: u16,
+    num_fats: u8,
+    root_entry_count: u16,
+    total_sectors_16: u16,
+    media: u8,
+    fat_size_16: u16,
+    sectors_per_track: u16,
+    num_heads: u16,
+    hidden_sectors: u32,
+    total_sectors_32: u32,
+    fat_size_32: u32,
+    ext_flags: u16,
+    fs_version: u16,
+    root_cluster: u32,
+    fs_info: u16,
+    bk_boot_sector: u16,
+    drive_number: u8,
+    boot_signature: u8,
+    volume_id: u32,
+    volume_label: String,
+    file_system_type: String,
+    signature: u16,
+}
+
 #[derive(Debug)]
 pub struct BpbGeneral {
     pub jmp_boot: [u8; 3],
@@ -80,6 +109,187 @@ pub enum BpbError {
     },
     InvalidSignature(u16),
     TooManyClustersForNonFat32Header,
+}
+
+impl Bpb2 {
+    pub fn parse(raw: &[u8]) -> Result<Self, BpbError> {
+        use std::convert::TryInto;
+        let jmp_boot = [raw[0], raw[1], raw[2]];
+        let oem_name = String::from_utf8_lossy(&raw[3..11]).to_string();
+        let bytes_per_sector = u16::from_le_bytes(raw[11..13].try_into().unwrap());
+        let sectors_per_cluster = raw[13];
+        let reserved_sector_count = u16::from_le_bytes(raw[14..16].try_into().unwrap());
+        let num_fats = raw[16];
+        let root_entry_count = u16::from_le_bytes(raw[17..19].try_into().unwrap());
+        let total_sectors_16 = u16::from_le_bytes(raw[19..21].try_into().unwrap());
+        let media = raw[21];
+        let fat_size_16 = u16::from_le_bytes(raw[22..24].try_into().unwrap());
+        let sectors_per_track = u16::from_le_bytes(raw[24..26].try_into().unwrap());
+        let num_heads = u16::from_le_bytes(raw[26..28].try_into().unwrap());
+        let hidden_sectors = u32::from_le_bytes(raw[28..32].try_into().unwrap());
+        let total_sectors_32 = u32::from_le_bytes(raw[32..36].try_into().unwrap());
+        let (
+            fat_size_32,
+            ext_flags,
+            fs_version,
+            root_cluster,
+            fs_info,
+            bk_boot_sector,
+            drive_number,
+            boot_signature,
+            volume_id,
+            volume_label,
+            file_system_type,
+        );
+        if (total_sectors_16 != 0) && (total_sectors_32 == 0) {
+            // FAT32
+            fat_size_32 = u32::from_le_bytes(raw[36..40].try_into().unwrap());
+            ext_flags = u16::from_le_bytes(raw[40..42].try_into().unwrap());
+            fs_version = u16::from_le_bytes(raw[42..44].try_into().unwrap());
+            root_cluster = u32::from_le_bytes(raw[44..48].try_into().unwrap());
+            fs_info = u16::from_le_bytes(raw[48..50].try_into().unwrap());
+            bk_boot_sector = u16::from_le_bytes(raw[50..52].try_into().unwrap());
+            for i in 52..64 {
+                if raw[i] != 0 {
+                    return Err(BpbError::UnexpectedNonZero { byte_index: i });
+                }
+            }
+            drive_number = raw[64];
+            if raw[65] != 0 {
+                return Err(BpbError::UnexpectedNonZero { byte_index: 65 });
+            }
+            boot_signature = raw[66];
+            volume_id = u32::from_le_bytes(raw[67..71].try_into().unwrap());
+            volume_label = String::from_utf8_lossy(&raw[71..82]).to_string();
+            file_system_type = String::from_utf8_lossy(&raw[82..90]).to_string();
+        } else if (total_sectors_16 == 0) && (total_sectors_32 != 0) {
+            // FAT12 or FAT16
+            fat_size_32 = 0;
+            ext_flags = 0;
+            fs_version = 0;
+            root_cluster = 0;
+            fs_info = 0;
+            bk_boot_sector = 0;
+            drive_number = raw[36];
+            if raw[37] != 0 {
+                return Err(BpbError::UnexpectedNonZero { byte_index: 37 });
+            }
+            boot_signature = raw[38];
+            volume_id = u32::from_le_bytes(raw[39..43].try_into().unwrap());
+            volume_label = String::from_utf8_lossy(&raw[43..54]).to_string();
+            file_system_type = String::from_utf8_lossy(&raw[54..62]).to_string();
+        } else {
+            return Err(BpbError::ExactlyOneTotalSectorsFieldMustBeZero {
+                total_sectors_16,
+                total_sectors_32,
+            });
+        }
+        if (fat_size_16 == 0) == (fat_size_32 == 0) {
+            return Err(BpbError::ExactlyOneFatSizeMustBeZero {
+                fat_size_16,
+                fat_size_32,
+            });
+        }
+        let signature = u16::from_le_bytes(raw[510..512].try_into().unwrap());
+        if signature != REQUIRED_SIGNATURE {
+            return Err(BpbError::InvalidSignature(signature));
+        }
+        Ok(Self {
+            jmp_boot,
+            oem_name,
+            bytes_per_sector,
+            sectors_per_cluster,
+            reserved_sector_count,
+            num_fats,
+            root_entry_count,
+            total_sectors_16,
+            media,
+            fat_size_16,
+            sectors_per_track,
+            num_heads,
+            hidden_sectors,
+            total_sectors_32,
+            fat_size_32,
+            ext_flags,
+            fs_version,
+            root_cluster,
+            fs_info,
+            bk_boot_sector,
+            drive_number,
+            boot_signature,
+            volume_id,
+            volume_label,
+            file_system_type,
+            signature,
+        })
+    }
+
+    fn total_sectors(&self) -> u32 {
+        if self.total_sectors_16 != 0 && self.total_sectors_32 == 0 {
+            self.total_sectors_16 as u32
+        } else {
+            debug_assert!(self.total_sectors_16 == 0 && self.total_sectors_32 != 0);
+            self.total_sectors_32
+        }
+    }
+
+    fn fat_size_in_sectors(&self) -> u32 {
+        if self.fat_size_16 != 0 && self.fat_size_32 == 0 {
+            self.fat_size_16 as u32
+        } else {
+            debug_assert!(self.fat_size_16 == 0 && self.fat_size_32 != 0);
+            self.fat_size_32
+        }
+    }
+
+    fn count_of_clusters(&self) -> u32 {
+        let root_dir_sectors = ((self.root_entry_count as u32 * 32)
+            + (self.bytes_per_sector as u32 - 1))
+            / self.bytes_per_sector as u32;
+        let total_sectors = if self.total_sectors_16 != 0 {
+            self.total_sectors_16 as u32
+        } else {
+            self.total_sectors_32
+        };
+        let data_sectors = total_sectors
+            - (self.reserved_sector_count as u32
+                + (self.num_fats as u32 * self.fat_size_in_sectors())
+                + root_dir_sectors);
+        data_sectors / self.sectors_per_cluster as u32
+    }
+
+    fn fat_type(&self) -> FatType {
+        let count_of_clusters = self.count_of_clusters();
+        if count_of_clusters < 4085 {
+            FatType::Fat12
+        } else if count_of_clusters < 65525 {
+            FatType::Fat16
+        } else {
+            FatType::Fat32
+        }
+    }
+
+    fn maximum_valid_cluster(&self) -> u32 {
+        self.count_of_clusters() + 1
+    }
+
+    fn root_directory_num_bytes(&self) -> usize {
+        debug_assert!((self.fat_type() == FatType::Fat32) == (self.root_entry_count == 0));
+        self.root_entry_count as usize * DIRECTORY_ENTRY_BYTES
+    }
+
+    fn fat_byte_range(&self) -> std::ops::Range<usize> {
+        let start = self.reserved_sector_count as usize * self.bytes_per_sector as usize;
+        let end = start + (self.fat_size_in_sectors() as usize * self.bytes_per_sector as usize);
+        start..end
+    }
+
+    fn cluster_byte_offset(&self) -> usize {
+        self.root_directory_num_bytes()
+            + ((self.reserved_sector_count as usize
+                + self.fat_size_in_sectors() as usize * self.num_fats as usize)
+                * self.bytes_per_sector as usize)
+    }
 }
 
 impl BpbGeneral {
@@ -381,7 +591,7 @@ impl Bpb {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum FatType {
     Fat12,
     Fat16,
