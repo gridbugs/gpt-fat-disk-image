071452a3fd5248d39a5e6033a1caa7312ab73352
Stephen Sherratt <stephen@sherra.tt>
2020-09-04 00:28:56 +1000

    Read takes output option

--- read/src/main.rs
+++ read/src/main.rs
@@ -1,6 +1,10 @@
+use std::fs::File;
+use std::io;
+
 struct Args {
     image_filename: String,
     cat_filename: String,
+    output: Box<dyn io::Write>,
 }
 
 impl Args {
@@ -10,8 +14,17 @@ impl Args {
             let {
                 image_filename = simon::opt("i", "image", "path to disk image", "PATH").required();
                 cat_filename = simon::opt("f", "file", "path within image of file to cat", "PATH").required();
+                output = simon::opt::<String>("o", "output", "output file path (omit for stdout)", "PATH");
             } in {
-                Self { image_filename, cat_filename }
+                Self {
+                    image_filename,
+                    cat_filename,
+                    output: if let Some(path) = output {
+                        Box::new(File::create(path).unwrap())
+                    } else {
+                        Box::new(io::stdout())
+                    },
+                }
             }
         })
         .with_help_default()
@@ -20,15 +33,13 @@ impl Args {
 }
 
 fn main() {
-    use std::fs::File;
-    use std::io;
     let Args {
         image_filename,
         cat_filename,
+        mut output,
     } = Args::parse();
     let mut image_file = File::open(image_filename).expect("unable to open file");
     let first_partition_byte_range = mini_gpt::first_partition_byte_range(&mut image_file).unwrap();
-    let mut output = io::stdout();
     mini_fat::read_file(
         &mut image_file,
         first_partition_byte_range,
