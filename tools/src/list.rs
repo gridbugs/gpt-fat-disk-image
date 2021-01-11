390483c6e1f0078b421c447ba2f951a5085c47dc
Stephen Sherratt <stephen@sherra.tt>
2021-01-12 09:48:15 +1100

    Add recursive list flag

--- tools/src/list.rs
+++ tools/src/list.rs
@@ -1,9 +1,13 @@
+use std::io;
+use std::path;
+
 mod error;
 
 struct Args {
     image_filename: String,
     list_filename: String,
     partition_only: bool,
+    recursive: bool,
 }
 
 impl Args {
@@ -14,11 +18,13 @@ impl Args {
                 list_filename = opt_opt("PATH", 'f').name("file").desc("path within image of file to list")
                     .with_default_parse("/");
                 partition_only = flag('p').name("partition-only").desc("expect image to be a partition instead of an entire disk");
+                recursive = flag('r').name("recursive").desc("recursively list directories");
             } in {
                 Self {
                     image_filename,
                     list_filename,
                     partition_only,
+                    recursive,
                 }
             }
         })
@@ -33,6 +39,7 @@ fn main() {
         image_filename,
         list_filename,
         partition_only,
+        recursive,
     } = Args::parse();
     env_logger::init();
     let mut image_file = File::open(image_filename).expect("unable to open file");
@@ -45,12 +52,53 @@ fn main() {
         &mut image_file,
         first_partition_byte_range,
     ));
-    match error::or_die(reader.lookup(&list_filename)) {
-        mini_fat::FatFile::Normal(_) => println!("{}", list_filename),
-        mini_fat::FatFile::Directory(directory) => {
-            for e in directory.entries() {
-                println!("{}", e.name());
+    if recursive {
+        error::or_die(recursive_list(&mut reader, &list_filename));
+    } else {
+        match error::or_die(reader.lookup(&list_filename)) {
+            mini_fat::FatFile::Normal(_) => println!("{}", list_filename),
+            mini_fat::FatFile::Directory(directory) => {
+                for e in directory.entries() {
+                    let name = e.name();
+                    if name != "." && name != ".." {
+                        println!("{}", name);
+                    }
+                }
             }
         }
     }
+}
+
+fn recursive_list<H: io::Seek + io::Read, P: AsRef<path::Path>>(
+    reader: &mut mini_fat::FatReader<H>,
+    path: P,
+) -> Result<(), mini_fat::Error> {
+    use std::collections::VecDeque;
+    let mut queue = VecDeque::new();
+    queue.push_back(path.as_ref().to_path_buf());
+    while let Some(path) = queue.pop_front() {
+        match reader.lookup(&path)? {
+            mini_fat::FatFile::Normal(_) => println!("{}", path.to_string_lossy()),
+            mini_fat::FatFile::Directory(directory) => {
+                let path_string = path.to_string_lossy().to_string();
+                println!("{}:", path_string);
+                for e in directory.entries() {
+                    let name = e.name();
+                    if name != "." && name != ".." {
+                        println!("  {}", name);
+                        if e.is_directory() {
+                            let path_string = if path_string == "/" {
+                                "".into()
+                            } else {
+                                path_string.clone()
+                            };
+                            queue.push_back(format!("{}/{}", path_string, name).into());
+                        }
+                    }
+                }
+                println!("");
+            }
+        }
+    }
+    Ok(())
 }
